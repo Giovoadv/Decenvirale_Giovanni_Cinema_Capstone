569427e781b2e8384cbd8f0508abd29e0cb2e559
giovo_adv@hotmail.com <giovo_adv@hotmail.com>
2024-08-28 15:43:23 -0500

    working on deployment

--- server/server.js
+++ server/server.js
@@ -16,12 +16,19 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 import { addFavourite } from "./usersController.js";
 
+
+
 dotenv.config({});
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(
   session({
